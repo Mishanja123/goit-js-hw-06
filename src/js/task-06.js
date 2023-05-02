c8d9220e8644bab68a13cdc18b4b3e8a90a4d35b
Misha <shevchenkom606@gmail.com>
2023-05-02 11:25:09 +0200

    remake 6exe

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -4,12 +4,14 @@ const input = document.querySelector('#validation-input')
 input.addEventListener('blur', onInputChange)
 
 function onInputChange(event) {
-    if (event.currentTarget.value.length >= input.dataset.value) {
+    if (event.currentTarget.value.length >= input.dataset.length) {
+         event.currentTarget.classList.remove('invalid')
         event.currentTarget.classList.add('valid')
         
     }
-    else{
-        event.currentTarget.classList.add('invalid')
+    else {
         event.currentTarget.classList.remove('valid')
+        event.currentTarget.classList.add('invalid')
+        
     }
 }
